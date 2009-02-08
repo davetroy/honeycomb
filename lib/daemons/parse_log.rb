dbf54634da750b3c30ca150528ed0c08735580d4
David Troy <dave@popvox.com>
2009-02-07 19:08:29 -0500

    Added runloop

--- lib/daemons/parse_log.rb
+++ lib/daemons/parse_log.rb
@@ -7,8 +7,13 @@ require File.dirname(__FILE__) + "/../../config/environment"
 
 Appearance.connection.execute("TRUNCATE TABLE appearances")
 
-File.readlines(logfile).each do |line|
-  line.grep(/DHCPACK/).each do |l|
-    Appearance.parse(l.chomp)
+$running = true; Signal.trap("TERM") { $running = false }
+
+while ($running) do
+  File.readlines(logfile).each do |line|
+    line.grep(/DHCPACK/).each do |l|
+      Appearance.parse(l.chomp)
+    end
   end
+  sleep 60
 end
