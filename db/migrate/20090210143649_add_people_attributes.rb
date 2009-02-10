618a6ebb8acd0ba805764778b4435263b41561ca
David Troy <davetroy@teflon.local>
2009-02-10 09:43:11 -0500

    Add present field to Person Add twitter_username to Person

---
+++ db/migrate/20090210143649_add_people_attributes.rb
@@ -0,0 +1,11 @@
+class AddPeopleAttributes < ActiveRecord::Migration
+  def self.up
+    add_column :people, :twitter_username, :string, :limit => 40
+    add_column :people, :present, :boolean    
+  end
+
+  def self.down
+    remove_column :people, :twitter_username
+    remove_column :people, :present
+  end
+end
