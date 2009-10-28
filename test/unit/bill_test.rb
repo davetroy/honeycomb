01d8d8ace47f28ce67b03f3199c9299eb4129049
Jonathan Julian <jonathan.m.julian@gmail.com>
2009-10-27 22:47:08 -0400

    calling wrong method breaks test - fixed

--- test/unit/bill_test.rb
+++ test/unit/bill_test.rb
@@ -6,7 +6,7 @@ class InvoiceTest < ActiveSupport::TestCase
     dave = people(:dave)
     dave.memberships.create(:plan => plans(:worker), :start_date => '1/31/2009', :end_date => '5/15/2009')
     assert_equal ['1/31/2009', '2/28/2009', '3/31/2009', '4/30/2009'].map { |d| Time.parse(d) }, dave.invoices.map(&:created_at)
-    assert_equal 175*4, dave.invoice_total
+    assert_equal 175*4, dave.bill_total
   end
   
   test "Basic membership per-incident invoices are created properly" do
