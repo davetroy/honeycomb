dd88b4fd3eb0062edd2bc9f77e95573472377c8e
Mike Subelsky <github@mikeshop.net>
2009-11-11 15:02:50 -0700

    Add activemerchant into the mix

--- config/environment.rb
+++ config/environment.rb
@@ -27,7 +27,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-
+  config.gem "activemerchant", :version => "1.4.2", :lib => 'active_merchant'
+  
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
