f553f972be64b77d0b256f4946ea6ea9ec9a63c8
Mike Subelsky <github@mikeshop.net>
2009-12-30 14:32:28 -0500

    add some date extensions

--- config/initializers/time.rb
+++ config/initializers/time.rb
@@ -3,3 +3,9 @@ class Time
     ((self.to_i + self.gmtoff) / 86400) - STARTING_DAY
   end
 end
+
+{ :short_date  => "%x",              # 04/13/10
+  :long_date   => "%a, %b %d, %Y"   # Tue, Apr 13, 2010 
+}.each do |k, v|
+  ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.update(k => v)
+end
