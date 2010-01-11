3120097498e77c9a0574ca8216f3632fa6d40ff4
David Troy <davetroy@teflon.local>
2010-01-10 22:40:25 -0500

    Tweak twitter oauth

--- config/initializers/twitteroauth.rb
+++ config/initializers/twitteroauth.rb
@@ -3,7 +3,7 @@ class TwitterOauth
   CONSUMER_TOKEN = "aVSiSKcPrLC27CDqe14cCg"
   CONSUMER_KEY = "UkRztJCb90YA4pHumzNoWMc4g4YEGR0mFqu9s6n6G8"
 
-  API = OAuth::Consumer.new(CONSUMER_TOKEN, CONSUMER_KEY, {
+  AUTH = OAuth::Consumer.new(CONSUMER_TOKEN, CONSUMER_KEY, {
          :site               => "http://api.twitter.com",
          :scheme             => :header,
          :http_method        => :post,
@@ -11,13 +11,19 @@ class TwitterOauth
          :access_token_path  => "/oauth/access_token",
          :authorize_path     => "/oauth/authorize"
         })
+  
+  API = OAuth::Consumer.new(CONSUMER_TOKEN, CONSUMER_KEY, {
+         :site               => "http://api.foursquare.com",
+         :scheme             => :header,
+         :http_method        => :post
+        })
             
   def self.get_request_token
-    API.get_request_token(:oauth_callback => "http://hive.beehivebaltimore.org/oauth/setup_twitter")
+    AUTH.get_request_token(:oauth_callback => "http://> hive.beehivebaltimore.org/oauth/setup_twitter")
   end
   
   def self.finish(request_token, oauth_verifier)
-    final_request_token = OAuth::RequestToken.new(API, request_token[:token], request_token[:secret])
+    final_request_token = OAuth::RequestToken.new(AUTH, request_token[:token], request_token[:secret])
     final_request_token.get_access_token(:oauth_verifier => oauth_verifier)
   end
   
