e38ea4ab57f9b03da6f122f7adc90f5210ac91c9
Mike Subelsky <github@mikeshop.net>
2010-01-13 13:16:28 -0500

    make the activity view a route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ ActionController::Routing::Routes.draw do |map|
   map.logout '/logout', :controller => :people, :action => :logout
   map.confirm_login '/confirm_login/:id', :controller => :people, :action => :confirm_login, :conditions => {:method => :post}
 
-  map.resources :people, :member => {:confirm_device_for => :get, :contact => :get, :login => :get, :email_password => :get }
+  map.resources :people, :member => { :activity => :get, :confirm_device_for => :get, :contact => :get, :login => :get, :email_password => :get }
     
   map.oauth_connect 'oauth/:action', :controller => "oauth"
   map.oauth 'oauth/:action', :controller => "oauth"
