62a82af971386d5839f5f304d881fc15f7652cdb
David Troy <davetroy@teflon.local>
2010-01-14 18:26:51 -0500

    Add update_profile template

--- app/models/person_mailer.rb
+++ app/models/person_mailer.rb
@@ -3,7 +3,7 @@ class PersonMailer < ActionMailer::Base
   default_url_options[:port] = DEFAULT_PORT
 
   helper_method :protect_against_forgery?
-  
+    
   def confirmation(device)
     content_type "text/html"
     recipients  device.person.email
@@ -17,6 +17,14 @@ class PersonMailer < ActionMailer::Base
     recipients  person.email
     from        'Honeycomb <info@beehivebaltimore.org>'
     subject     "[Beehive Baltimore] Reset your password"
+    body        :person => person
+  end
+
+  def update_profile(person)
+    content_type "text/html"
+    recipients  person.email
+    from        'Honeycomb <info@beehivebaltimore.org>'
+    subject     "[Beehive Baltimore] Welcome to 2010 - Please setup your profile"
     body        :person => person
   end
   
