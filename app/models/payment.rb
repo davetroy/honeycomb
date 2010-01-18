02b361f825ae4950823c56c7bae80ce9e6aaa347
Mike Subelsky <github@mikeshop.net>
2010-01-18 10:23:22 -0500

    Specify relationship between payments and person

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,7 +2,9 @@ class Payment < ActiveRecord::Base
   validates_presence_of :person_id
   validates_numericality_of :amount, :greater_than_or_equal_to => 0
   serialize :details
-
+  belongs_to :person
+  delegate :show_name, :to => :person, :prefix => true
+  
   # amount is stored in the database as an integer to avoid floating point issues,
   # so we have a getter and setter to normalize the amounts
   
@@ -10,6 +12,11 @@ class Payment < ActiveRecord::Base
     read_attribute(:amount) / 100.0
   end
 
+  # TODO instead of integer_amount, we should be passing around ActiveMerchant Money objects which take care of this conversion
+  def integer_amount
+    read_attribute(:amount)
+  end           
+  
   def amount=(value)
     write_attribute(:amount,(value.to_f * 100).to_i)
   end
